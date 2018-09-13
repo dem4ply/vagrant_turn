0afb5818f556f09d7e034167bccdbaf2347e5518
dem4ply <dem4ply@gmail.com>
2018-09-13 14:11:09 -0500

    add the template for partners

--- provision/provision_elastic.sh
+++ provision/provision_elastic.sh
@@ -1,8 +1,26 @@
 FOLDER_PROVISION="/home/vagrant/provision"
 
-sleep 10
+while [ $( curl -I localhost:9200 2> /dev/null | grep -q -v "200 OK" ) ]
+do
+	sleep 10
+done
 
-curl localhost:9200/
-curl localhost:9200/_template/turn_api__partner_worker -XPUT -d@"$FOLDER_PROVISION/partner_worker_template.json" -H 'content-type: application/json'
-curl localhost:9200/_template/turn_api__log -XPUT -d@"$FOLDER_PROVISION/log.template.json" -H 'content-type: application/json'
-curl localhost:9200/_template/turn_api__resource_event -XPUT -d@"$FOLDER_PROVISION/resource_event.template.json" -H 'content-type: application/json'
+sleep 2
+
+curl -v localhost:9200/ 2> /dev/null
+
+curl -v localhost:9200/_template/turn_api__partner_worker -XPUT \
+	-d@"$FOLDER_PROVISION/partner_worker_template.json" \
+	-H 'content-type: application/json' 2> /dev/null
+
+curl -v localhost:9200/_template/turn_api__log -XPUT \
+	-d@"$FOLDER_PROVISION/log.template.json" \
+	-H 'content-type: application/json' 2> /dev/null
+
+curl -v localhost:9200/_template/turn_api__resource_event -XPUT \
+	-d@"$FOLDER_PROVISION/resource_event.template.json" \
+	-H 'content-type: application/json' 2> /dev/null
+
+curl -v localhost:9200/_template/turn_api__partner -XPUT \
+	-d@"$FOLDER_PROVISION/partner.template.json" \
+	-H 'content-type: application/json' 2> /dev/null
